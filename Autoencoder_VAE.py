fa7df4265e4ffbf5bca061fc8a4e79a629496788
uif29454 <alexandru-mihai.2.lungu@continental-corporation.com>
2025-01-28 16:48:01 +0200

    push with bug

--- Autoencoder_VAE.py
+++ Autoencoder_VAE.py
@@ -1,3 +1,5 @@
+from uu import decode
+
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.python.ops.gen_batch_ops import batch
@@ -10,24 +12,30 @@ def generate_time_series(num_series, seq_length):
     x = np.sin(t) + 0.1 * np.random.randn(num_series, seq_length)  # Valori observate (sinus cu zgomot)
     return t, x
 
-def plot_data():
+def plot_data(x, x_predict):
 
     plt.figure(figsize=(12, 6))
-    for i in range(5):
-        plt.subplot(5, 1, i + 1)
-        plt.plot(t, x[i], label=f"Seria {i + 1}")
-        plt.legend(loc="upper right")
+    plt.plot(t, x, label=f"Seria train")
+    plt.plot(t, x_predict, label=f"Seria predict")
+    plt.legend(loc="upper right")
     plt.tight_layout()
     plt.show()
 
-    def re_parameterize(mu, logvar):
-        # calculate the variance for distribution
-        std = np.exp(0.5 * logvar)
-        eps = np.random.randn(*mu.shape)
-        return mu + eps * std
+def re_parameterize(mu, logvar):
 
-    def reconstruction_loss(x, x_recon):
-        return np.mean((x - x_recon) ** 2, axis=1)
+    # calculate the variance for distribution
+    # return the point created with the distribution parameters
+
+    std = np.exp(0.5 * logvar)
+    eps = np.random.randn(*mu.shape)
+    return mu + eps * std
+
+def reconstruction_loss(x, x_recon):
+    return np.mean((x - x_recon) ** 2)
+
+def kl_divergence(mu, logvar):
+    return 0.5 * np.sum(1 + logvar - mu**2 - np.exp(logvar))
+
 
 if __name__ == '__main__':
 
@@ -37,7 +45,7 @@ if __name__ == '__main__':
 
     print("t shape:", t.shape)  # (100,)
     print("x shape:", x.shape)  # (1000, 100)
-    plot_data()
+    # plot_data()
 
     # Params
     input_dim = seq_length
@@ -49,4 +57,36 @@ if __name__ == '__main__':
 
     encoder = Encoder(input_dim, hidden_dim, latent_dim)
     decoder = Decoder(latent_dim, hidden_dim, input_dim)
+
+
+    for epochs in range(epochs):
+        for i in range(0, num_series, batch_size):
+
+            x_batch = x[i]
+
+            # forward
+            mu, logvar = encoder.forward(x_batch)
+            z = re_parameterize(mu, logvar)
+            x_recon = decoder.forward(z)
+
+            # plot_data(x_batch, x_recon)
+
+            # compute loss
+            recon_loss = reconstruction_loss(x_batch, x_recon)
+            kl_loss = kl_divergence(mu, logvar)
+            total_loss =np.mean(recon_loss + kl_loss)
+            print("Loss:", total_loss)
+
+            grad_recon = (x_recon - x_batch)
+            decoder.backward(z, grad_recon)
+
+
+
+
+
+
+
+
+
+
 
--- Encoder_TS.py
+++ Encoder_TS.py
@@ -10,14 +10,13 @@ class Encoder:
 
         # the neurons that produce the mean of the latent distribution
         self.W_mu = np.random.randn(hidden_dim, latent_dim) * 0.01
-        self.b_mu = np.zeros(hidden_dim)
+        self.b_mu = np.zeros(latent_dim)
 
         # the neurons that produce the log of the variance (square of the standard deviation)
         self.W_logvar = np.random.randn(hidden_dim, latent_dim) * 0.01
         self.b_logvar = np.zeros(latent_dim)
 
     def forward(self, x):
-
 
         h = np.tanh(np.dot(x, self.W1) + self.b1)
         mu = np.dot(h, self.W_mu) + self.b_mu
@@ -37,5 +36,21 @@ class Decoder():
     def forward(self, z):
 
         h = np.tanh(np.dot(z, self.W1) + self.b1)
-        x_recon = np.dot(h, self.W2) + self.W2
+        x_recon = np.dot(h, self.W2) + self.b2
         return x_recon
+
+    def backward(self, z, grad_recon):
+
+        grad_recon = grad_recon.reshape(grad_recon.shape[0],1)
+        grad_h = np.dot(grad_recon.T, self.W2.T)
+        grad_h *= (1 - np.tanh(np.dot(z, self.W1) + self.b1) ** 2)  # Derivative of tanh
+
+        # reshape
+        z = z.reshape(z.shape[0], 1)
+
+        # Gradients for weights and biases
+        self.grad_W1 = np.dot(z, grad_h.T)
+        self.grad_b1 = np.sum(grad_h, axis=0)
+        self.grad_W2 = np.dot(np.tanh(np.dot(z.T, self.W1) + self.b1).T, grad_recon)
+        self.grad_b2 = np.sum(grad_recon, axis=0)
+
